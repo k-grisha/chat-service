d906e37dd0cafb01b7001838319c6c98b9ac4465
Grigory Kiladze <ryhor.kiladze@quandoo.com>
2020-12-13 16:00:12 +0100

    entity adjusted

--- src/main/java/chat/onmap/chatservice/model/Message.java
+++ src/main/java/chat/onmap/chatservice/model/Message.java
@@ -1,7 +1,10 @@
 package chat.onmap.chatservice.model;
 
 import java.util.UUID;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -22,6 +25,8 @@ public class Message {
     private long id;
     private UUID sender;
     private UUID recipient;
+    @Enumerated(EnumType.STRING)
     private MessageType type;
+    @Column(columnDefinition = "TEXT")
     private String body;
 }
